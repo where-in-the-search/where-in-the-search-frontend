f9908a2317221bab2f92ebe50580229e2fb98d8c
Soraya Benson <soraya.benson@gmail.com>
2021-03-11 19:34:11 -0800

    css

--- src/About/AboutPage.js
+++ src/About/AboutPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './AboutPage.css';
 
+{/* <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */}
+
 export default class AboutPage extends Component {
     render() {
         return (
